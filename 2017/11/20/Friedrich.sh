014fa2e5ede4099dde3fc22d883a0a323aa8be27
NfN Orange <orange@ff4500.red>
2017-11-20 18:57:20 +0000

    removed the ls because why was that there

--- 2017/11/20/Friedrich.sh
+++ 2017/11/20/Friedrich.sh
@@ -5,5 +5,4 @@
 SCRIPTDIR=$(dirname $0)
 pushd $SCRIPTDIR 
 ../../../Tools/tweet.sh/tweet.sh post '"He who fights with monsters should look to it that he himself does not become a monster. And if you gaze long into an abyss, the abyss also gazes into you." - Friedrich Nietzsche, Beyond Good and Evil, Aphorism 146 #BoaDaBoA'
-ls
 popd
