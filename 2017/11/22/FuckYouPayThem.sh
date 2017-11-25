83a88072d30b40a4eead1c0bd7b84fdf50635ba1
NfN Orange <orange@ff4500.red>
2017-11-25 00:57:55 +0000

    Fixed Fuck You Pay them

--- 2017/11/22/FuckYouPayThem.sh
+++ 2017/11/22/FuckYouPayThem.sh
@@ -23,32 +23,39 @@ lynx -dump -listonly 'https://twitter.com/search?f=tweets&q=%20can%20we%20use%20
 
 fuck(){
     # ../../../Tools/tweet.sh/tweet.sh reply $1 "Fuck you, Pay them."
-    t reply $1 "Fuck you, Pay them."
+    t reply $1 "Fսck yoս, Pay them."
+    echo "sleeping" 
+    sleep $(( (10 + $RANDOM )  % 120 ))s
 }
 
+THISROUNDPEOPLE=$(mktemp)
 pushd $SCRIPTDIR 
 # For every tweet check if it has been seen before 
 while read id; do
     if ! egrep -q "^$id" .seenids; then
         # Grab tweet contents and store it 
         ../../../Tools/tweet.sh/tweet.sh get $id > $TWEETCONTENT
-        if ! egrep -q "pay" <(jq .full_text $TWEETCONTENT); then
-            if [ "$(jq .user.followers_count $TWEETCONTENT)" -gt "1000" ]; then
-                fuck $id
-            else
-                # Currently fall back to t for user checking, if not then think they mean well
-                if [ -e "/usr/local/bin/t" ]; then 
-                    INREPLY=$(jq .in_reply_to_screen_name $TWEETCONTENT | sed -e 's/"//g;s/\(.*\)/@\1/g')
-                    jq .user.description $TWEETCONTENT | grep -o "@.[^ ]*" >> $SCREENNAMES
-                    jq .full_text $TWEETCONTENT | grep -o "@.[^ ]*" >> $SCREENNAMES
-                    sed -i "/[.\/]/d;/${INREPLY}/d;s/[^a-zA-Z0-9_-@]//g;s/\"//g" $SCREENNAMES
-                    COUNT=0
-                    while read user; do 
-                        FOLLOWERCOUNT=$(t whois -c $user | cut -d, -f7 | tail -1)
-                        : $(( COUNT += FOLLOWERCOUNT )) 
-                    done < <(sort $SCREENNAMES | uniq | sed "/^$/d")
-                    if [ "${COUNT}" -gt "2000" ]; then 
-                        fuck $id
+        if ! egrep -q "$(jq .user.screen_name $TWEETCONTENT)" $THISROUNDPEOPLE ; then
+            if ! egrep -q "pay" <(jq .full_text $TWEETCONTENT); then
+                if [ "$(jq .user.followers_count $TWEETCONTENT)" -gt "1000" ]; then
+                    fuck $id
+                    jq .user.screen_name $TWEETCONTENT >> $THISROUNDPEOPLE
+                else
+                    # Currently fall back to t for user checking, if not then think they mean well
+                    if [ -e "/usr/local/bin/t" ]; then 
+                        INREPLY=$(jq .in_reply_to_screen_name $TWEETCONTENT | sed -e 's/"//g;s/\(.*\)/@\1/g')
+                        jq .user.description $TWEETCONTENT | grep -o "@.[^ ]*" >> $SCREENNAMES
+                        jq .full_text $TWEETCONTENT | grep -o "@.[^ ]*" >> $SCREENNAMES
+                        sed -i "/[.\/]/d;/${INREPLY}/d;s/[^a-zA-Z0-9_-@]//g;s/\"//g" $SCREENNAMES
+                        COUNT=0
+                        while read user; do 
+                            FOLLOWERCOUNT=$(t whois -c $user | cut -d, -f7 | tail -1)
+                            : $(( COUNT += FOLLOWERCOUNT )) 
+                        done < <(sort $SCREENNAMES | uniq | sed "/^$/d")
+                        if [ "${COUNT}" -gt "2000" ]; then 
+                            fuck $id
+                            jq .user.screen_name $TWEETCONTENT >> $THISROUNDPEOPLE
+                        fi
                     fi
                 fi
             fi
