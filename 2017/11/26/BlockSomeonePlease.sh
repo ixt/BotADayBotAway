f9665b666d7637c526575d03152f0219e67fafe7
NfN Orange <orange@ff4500.red>
2017-11-26 00:31:17 +0000

    Another day, another trump bot fix, hopefully that fixes it getting stuck, the new bot blocks a random person by their id

---
+++ 2017/11/26/BlockSomeonePlease.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# https://twitter.com/minicacher/status/1879457115
+# Although it does not crawl, it does randomly block random users 
+SCRIPTDIR=$(dirname $0)
+
+idToHandle(){
+    ID=$1
+    USER=$(wget -qO- "https://twitter.com/intent/user?user_id=$1" | \
+    egrep '<a class="button follow" href="/intent/follow\?screen_name=' | \
+    cut -d"=" -f4 | sed 's/">//g' )
+
+    [ "${USER}" == "" ] && return 1
+    echo ${USER}
+}
+
+pushd $SCRIPTDIR 
+STATUS="0"
+while [ "$STATUS" == "0" ]; do
+    HANDLE=$(./IdToHandle.sh $(( $RANDOM * ( $RANDOM % $(date +%Y) ) + $(date +"%d + %m") )))
+    [ ! "${HANDLE}" == "" ] && STATUS="1"
+done
+t block "$HANDLE"
+popd
