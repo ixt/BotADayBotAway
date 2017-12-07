d7cf7e172f4b695f3f91f68eefa53b50cea75bb5
NfN Orange <orange@ff4500.red>
2017-12-07 19:52:10 +0000

    Fixing bee bot, was printing whole file instead of a line

--- 2017/11/25/BecauseBeesDontCare.sh
+++ 2017/11/25/BecauseBeesDontCare.sh
@@ -3,5 +3,5 @@
 # Tweets a line of the bee movie script every 12 hours in order
 SCRIPTDIR=$(dirname $0)
 pushd $SCRIPTDIR 
-../../../Tools/tweet.sh/tweet.sh post "$(sed 1p .lines)" && sed -i 1d .lines
+../../../Tools/tweet.sh/tweet.sh post "$(sed -n 1p .lines)" && sed -i 1d .lines
 popd
