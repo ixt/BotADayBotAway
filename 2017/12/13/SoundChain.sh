279e1f11fb6f8c0655c6659a198502a647848d3b
NfN Orange <orange@ff4500.red>
2017-12-21 00:04:55 +0000

    Hopefully a good fix for SoundChain

--- 2017/12/13/SoundChain.sh
+++ 2017/12/13/SoundChain.sh
@@ -20,18 +20,22 @@ echo "$LASTUSER" >> .seenusers
 # Get a list of recent likes 
 lynx -dump -listonly https://soundcloud.com/$LASTUSER/likes | cut -d. -f2- | grep "://soundcloud" | sed -e "s/^ //g;/oembed/d;/.xml/d;/\/$/d;/popular\/searches/d" | cut -d/ -f4- | grep "/" | sort > $listOfLinks
 
+
 # For every line, get that user's likes and give it a value, this prevents dead ends 
 while read LINE; do
 	USER=$(cut -d/ -f1 <<< "$LINE" )
-	VALUE=$(lynx -dump -listonly https://soundcloud.com/$USER/likes | cut -d. -f2- | grep "://soundcloud" | sed -e "s/^ //g;/oembed/d;/.xml/d;/\/$/d;/popular\/searches/d" | cut -d/ -f4- | grep "/" | wc -l )
+	VALUE=$(lynx -dump -listonly https://soundcloud.com/$USER/likes | cut -d. -f2- | grep "://soundcloud" | sed -e "s/^ //g;/oembed/d;/.xml/d;/\/$/d;/popular\/searches/d" | cut -d/ -f4- | grep "/" | sed -e "@^${USER}/@d" | wc -l )
 	echo $LINE $VALUE >> $values
 done < $listOfLinks
 
 # Delete seen users from the list and those without any likes
-sed -i -e "/ 0$/d" $values
+sed -i -e "/ [0-1]$/d" $values
 while read user; do
 	sed -i -e "/${user}/d" $values
 done < .seenusers
+
+# Remove those self-cremating assholes
+sed -i -e "/${LASTUSER}/d" $values
 
 # Choose a track 
 FINALURL="https://soundcloud.com/$(shuf $values | head -1 | cut -d" " -f1)"
@@ -40,5 +44,5 @@ FINALURL="https://soundcloud.com/$(shuf $values | head -1 | cut -d" " -f1)"
 echo $(cut -d/ -f4 <<< "$FINALURL") > .lastuser
 
 # And post
-../../../Tools/tweet.sh/tweet.sh post "$LASTUSER liked $FINALURL #SoundChain"
+echo "$LASTUSER liked $FINALURL #SoundChain"
 popd
