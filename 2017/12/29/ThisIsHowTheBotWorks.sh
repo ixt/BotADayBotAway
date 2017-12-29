e5abcca28034e0c9519cbbdc741e00c940297a60
NfN Orange <orange@ff4500.red>
2017-12-29 21:40:41 +0000

    Make it say the intro tweet instead

--- 2017/12/29/ThisIsHowTheBotWorks.sh
+++ 2017/12/29/ThisIsHowTheBotWorks.sh
@@ -1,3 +1,4 @@
+20171229 - ThisIsHowTheBotWorks.sh - 
 #!/bin/bash
 # How this bot works #BotADayBotAway 
 t=$(mktemp --suffix=.sh)
@@ -5,3 +6,4 @@ echo \\\\x27 | xargs -i@ echo -e twurl -d @status= > $t
 cat $0>>$t
 printf "\x27 /1.1/statuses/update.json">>$t
 bash $t
+- https://ff4500.red/projects/BoaDaBoA/
