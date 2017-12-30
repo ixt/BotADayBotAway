e892a7c09fb1ab840f6855a55a1c56c47e17dbeb
NfN Orange <orange@ff4500.red>
2017-12-30 21:16:18 +0000

    Hash Of the day script added

---
+++ 2017/12/30/HashOfTheDay.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -euo pipefail
+# Tweets out the hash of todays date using 4 different hashing algos
+SHA1=$(date +%Y%m%d | sha1sum | cut -d" " -f1)
+MD5=$(date +%Y%m%d | md5sum | cut -d" " -f1)
+SHA256=$(date +%Y%m%d | sha256sum | cut -d" " -f1)
+SHA384=$(date +%Y%m%d | sha384sum | cut -d" " -f1)
+twurl -d "status=The SHA1 Hash of the day: 
+$SHA1
+" /1.1/statuses/update.json
+sleep 1s
+twurl -d "status=The MD5 Hash of the day: 
+$MD5
+" /1.1/statuses/update.json
+sleep 5s
+twurl -d "status=The SHA256 Hash of the day: 
+$SHA256
+" /1.1/statuses/update.json
+sleep 256s
+twurl -d "status=The SHA384 Hash of the day: 
+" /1.1/statuses/update.json
