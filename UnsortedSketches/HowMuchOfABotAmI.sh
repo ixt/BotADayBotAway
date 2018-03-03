6a1162ed48a64efd40550c5b5e42001daa93d7a8
NfN Orange <orange@ff4500.red>
2018-03-03 23:32:24 +0000

    More reformatting

--- UnsortedSketches/HowMuchOfABotAmI.sh
+++ UnsortedSketches/HowMuchOfABotAmI.sh
@@ -8,16 +8,35 @@ PYTEMP=$(mktemp)
 TEMP=$(mktemp)
 
 # Get Current Profiles keys and info
-consumer_key=$(head -5 ~/.trc | tail -1 | sed -e "s/-//g" -e "s/[[:space:]]//g")
-tail -n+$(sed -n "/$consumer_key/=" ~/.trc | head -2 | tail -1) ~/.trc | head -6 > $TEMP
+consumer_key=$(head -5 ~/.trc \
+	| tail -1 \
+	| sed -e "s/-//g" -e "s/[[:space:]]//g")
+tail -n+$(sed -n "/$consumer_key/=" ~/.trc \
+	| head -2 \
+	| tail -1) ~/.trc \
+	| head -6 >$TEMP
 
-consumer_secret=$(grep "consumer_secret" $TEMP | cut -d: -f2 | xargs echo)
-access_token=$(grep "token" $TEMP | cut -d: -f2 | xargs echo)
-access_token_secret=$(grep "secret:" $TEMP | tail -1 | cut -d: -f2 | xargs echo)
-username=$(grep "username" $TEMP | tail -1 | cut -d: -f2 | xargs echo)
+consumer_secret=$(grep "consumer_secret" $TEMP \
+	| cut -d: -f2 \
+	| xargs echo)
+
+access_token=$(grep "token" $TEMP \
+	| cut -d: -f2 \
+	| xargs echo)
+
+access_token_secret=$(grep "secret:" $TEMP \
+	| tail -1 \
+	| cut -d: -f2 \
+	| xargs echo)
+
+username=$(grep "username" $TEMP \
+	| tail -1 \
+	| cut -d: -f2 \
+	| xargs echo)
+
 mashape_key=$(cat ~/.mashapekey)
 
-cat <<EOF > $PYTEMP
+cat <<EOF >$PYTEMP
 #!/bin/python
 import botometer
 
@@ -39,8 +58,8 @@ EOF
 BOTSCOREENG=""
 BOTSCOREUNI=""
 until [[ $BOTSCOREENG -gt 0 ]]; do
-    IFS=, read -a values <<< $(python $PYTEMP | cut -d"{" -f5 | sed -e "s/[a-z:' }]//g")
-    BOTSCOREENG=$(bc -l <<< "${values[0]} * 100" | cut -d. -f1)
-    BOTSCOREUNI=$(bc -l <<< "${values[1]} * 100" | cut -d. -f1)
+	IFS=, read -a values <<<$(python $PYTEMP | cut -d"{" -f5 | sed -e "s/[a-z:' }]//g")
+	BOTSCOREENG=$(bc -l <<<"${values[0]} * 100" | cut -d. -f1)
+	BOTSCOREUNI=$(bc -l <<<"${values[1]} * 100" | cut -d. -f1)
 done
 t update "My bot score according to Botometer (botometer.iuni.iu.edu) is $BOTSCOREENG%, ignoring the english specific stuff it is $BOTSCOREUNI%. Have a nice day."
--- UnsortedSketches/Water.sh
+++ UnsortedSketches/Water.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 RUNNINGTOTAL=0
 TEMP=$(mktemp)
-/usr/local/bin/t search all "Drink * glasses of water" -c | cut -d"," -f4- | tail -n+2 | grep "\s[0-9]*\sglasses\s" | sed "s/.*\(\s[0-9]*\sglasses\).*/\1/" | cut -d" " -f2 > $TEMP
+
+# Search for all the drink glasses of water tweets and extract the number of glasses
+/usr/local/bin/t search all "Drink * glasses of water" -c \
+    | cut -d"," -f4- \
+    | tail -n+2 \
+    | grep "\s[0-9]*\sglasses\s" \
+    | sed "s/.*\(\s[0-9]*\sglasses\).*/\1/" \
+    | cut -d" " -f2 >$TEMP
+
+# Add up all the glasses
 while read line; do
-    : $(( RUNNINGTOTAL += line ))
-done < $TEMP
-LINES=$(cat $TEMP | wc -l )
-/usr/local/bin/t update "$(echo 'Remember to drink '$(( RUNNINGTOTAL / $LINES ))' glasses of water a day! #PSA')"
+	: $((RUNNINGTOTAL += line))
+done <$TEMP
+
+LINES=$(cat $TEMP | wc -l)
+
+/usr/local/bin/t update "$(echo 'Remember to drink '$((RUNNINGTOTAL / $LINES))' glasses of water a day! #PSA')"
