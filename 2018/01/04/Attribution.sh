bdddc486f1fe4c84807e5277d2424cc50455d5a2
NfN Orange <orange@ff4500.red>
2018-05-08 00:15:38 +0000

    Changing attribution because of ToS

--- 2018/01/04/Attribution.sh
+++ 2018/01/04/Attribution.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-t update "This account is based on the article by Gordy Megroz for @outsidemagizine https://www.outsideonline.com/2142236/perfect-day"
+t update "This bot is based on the article by Gordy Megroz for ousidemagizine https://www.outsideonline.com/2142236/perfect-day"
