4ac271bac0188fe685ea2331946ca8ac840f576c
NfN Orange <orange@ff4500.red>
2021-12-16 14:26:31 +0000

    Added a bit better of parsing for the id of the cyberkid

--- 2021/12/12/ChildTraffking.sh
+++ 2021/12/12/ChildTraffking.sh
@@ -16,12 +16,13 @@ echo $fullresults > $temp
 
 while IFS=, read -a kid; do
     if [ "${kid[0]}" != "" ]; then
-        message="CyberKid ${kid[0]} just sold for ${kid[1]}" 
+        id=${kid[0]#*\#}
+        message="CyberKid #${id} just sold for ${kid[1]}" 
         hashes=$(sha256sum <<<"$message")
 
         if ! grep -q "$message" messages.txt; then
-            echo "$message" | tee -a messages.txt
-            twurl -u CyberTraffkt -X POST -H api.twitter.com "/1.1/statuses/update.json?status=$message https://objkt.com/asset/cyberkidzclub/${kid[0]//#/}"
+            echo "$message" # | tee -a messages.txt
+            # twurl -u CyberTraffkt -X POST -H api.twitter.com "/1.1/statuses/update.json?status=$message https://objkt.com/asset/cyberkidzclub/${kid[0]//#/}"
         else
             echo "seen $message"
         fi
@@ -31,7 +32,7 @@ done < <(sed -e "s/@: /\n/g;s/CyberKidz Club1xCyberKidz Club //g;s/tz1Z5...x7dRA
 
 if [[ $(wc -l < messages.txt) -gt 56 ]]; then
     echo "cutting"
-    tail -n 27 messages.txt > $temp
+    tail -n 28 messages.txt > $temp
     cp $temp messages.txt
 fi
 
